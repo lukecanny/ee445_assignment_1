62e97d154c4b0b49d992c7e7d774c6e220648098
lukecanny <lukecanny@protonmail.ch>
2022-10-25 19:38:53 +0100

    Completed exercise 3 part (i), plotted Impulse Response of the Filter and Magnitude Response

--- exercise_3.m
+++ exercise_3.m
@@ -8,8 +8,8 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Specifying Filters
-b1 = [0.16, -0.48, 0.48, -0.16]; % X Side
-a1 = [1, 0.13, 0.52, 0.3];       % Y Side
+b1 = [0.16 -0.48 0.48 -0.16]; % X Side
+a1 = [1 0.13 0.52 0.3];       % Y Side
 
 b2 = [0.634, 0, -0.634]; 
 a2 = [1, 0, -0.268]; 
@@ -19,3 +19,15 @@ a3 = [1, 0, 0.268];
 
 b4 = [0.634, -5, 10]; 
 a4 = [10, -5, 1]; 
+
+% Part (i)
+freqz(b1,a1);
+n = [0:25];
+h = impz(b1,a1,n);
+figure
+stem(n,h,'linewidth',2);
+grid on;
+xlabel('Sample Index (n)');
+ylabel('Magnitude of Impulse Response');
+title('Impulse Response of the Filter');
+
