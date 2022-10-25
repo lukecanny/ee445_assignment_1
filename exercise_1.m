306828d84b1ba44cc6e50166a41493723c446f53
lukecanny <lukecanny@protonmail.ch>
2022-10-25 15:52:22 +0100

    Applied freqz function to calculate magnitude response. Converted to dB and plotted results

--- exercise_1.m
+++ exercise_1.m
@@ -12,3 +12,17 @@ b = [1 0.4];
 a = [1 -1.5*cos(pi/8) 0.96];
 n = 1024;
 fs = 12000;
+
+% Applying freqz function
+[h, f] = freqz(b,a,n,fs);
+%  returns the n-point frequency response vector h and the corresponding 
+% angular frequency vector w for the digital filter with transfer function 
+% coefficients stored in b and a
+
+% Converting frequency response vector to decibels (h is complex)
+hDB = 20*log(abs(h));
+
+% Plotting data
+plot(f, hDB);
+xlabel("Frequency (Hz)");
+ylabel("Magnitude (dB)");
